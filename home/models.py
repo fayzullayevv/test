a0f384920bf3348f7a8cd49b9582222ff3d26397
fayzullayevv <fayzullayevfayozbek20@gmail.com>
2023-06-19 17:09:04 +0500

    modells

--- home/models.py
+++ home/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class HomeModel(models.Model):
+class RegisterModell(models.Model):
   name = models.CharField(max_length=100)
   surname = models.CharField(max_length=100)
-  email = models.EmailField(max_length=100)
+  school = models.PositiveBigIntegerField()
+  clas = models.PositiveBigIntegerField()
+  age = models.PositiveBigIntegerField()
+  course = models.CharField(max_length=100)
 
   def __str__(self):
     return self.name
+  
+
--- home/views.py
+++ home/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
-from .models import HomeModel
+from .models import RegisterModell
 import requests as r
 # Create your views here.
 
+
+
 def HomePage(request):
   if request.method == 'POST':
-    name = request.POST.get('name')
-    surname = request.POST.get('surname')
-    email = request.POST.get('email')
-    obj = HomeModel.objects.create(name=name,surname=surname,email=email)
+    name = request.POST.get('first')
+    surname = request.POST.get('last')
+    school = request.POST.get('school')
+    clas = request.POST.get('class')
+    age = request.POST.get('age')
+    course = request.POST.get('course')
+    obj = RegisterModell.objects.create(name=name,surname=surname,school=school,clas=clas,age=age,course=course)
     obj.save()
 
     token = "6129236243:AAEfiaGL4q5esogOfv6CPPqJa9KM-HmhuI8"
-    text = f"Sizga xabar yuborishdi: \nIsm: {request.POST.get('name')} \nFamilya: {request.POST.get('surname')} \nEmail: {request.POST.get('email')}"
+    text = f"Sizga xabar yuborishdi: \nFirst Name: {request.POST.get('first')} \nLast Name: {request.POST.get('last')} \nSchool: {request.POST.get('school')} \nClass: {request.POST.get('class')} \nAge: {request.POST.get('age')} \nCourse: {request.POST.get('course')}"
     url = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id='
     r.get(url + str(5469101117) + '&text=' + text)
   return render(request,'index.html')
